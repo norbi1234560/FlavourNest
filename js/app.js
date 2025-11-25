717ff4738570221b92588bbf64ab30f9a0ec7c0b
norbi1234560 <repanorbi.2006@gmail.com>
2025-11-25 07:28:27 +0100

    delete

--- js/app.js
+++ js/app.js
@@ -68,29 +68,6 @@
     'http',
     function($scope, $state, $timeout,http) {
       console.log("Home")
-
-      http.request("./php/base.php")
-      .then(response=>{
-        console.log(response);
-        let keys=Object.keys(response[0]);
-        let thead=document.querySelector("thead tr");
-        let tbody=document.querySelector("tbody");
-        keys.forEach(x=>{
-          let th=document.createElement("th");
-          thead.append(th);
-          th.innerText=x;
-        })
-        response.forEach(row=>{
-          let tr=document.createElement("tr");
-          tbody.append(tr);
-          keys.forEach(key=>{
-            let td=document.createElement("td");
-            tr.append(td);
-            td.innerText=row[key];
-          })
-        })
-      })
-      .catch(e=> error.log(e));
     }
   ])
 
