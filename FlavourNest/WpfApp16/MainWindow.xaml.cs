1b8e946794026c73c053f5c24127be287636d24f
norbi1234560 <repanorbi.2006@gmail.com>
2025-12-09 07:33:16 +0100

    new c#

---
+++ FlavourNest/WpfApp16/MainWindow.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System.Windows;
+
+namespace FlavourNest
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Dashboard_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabs.SelectedIndex = 0;
+        }
+
+        private void RecipeManagement_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabs.SelectedIndex = 1;
+        }
+
+        private void Users_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabs.SelectedIndex = 2;
+        }
+
+        private void Categories_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabs.SelectedIndex = 3;
+        }
+
+        private void Analytics_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabs.SelectedIndex = 4;
+        }
+
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            MainTabs.SelectedIndex = 5;
+        }
+    }
+}
