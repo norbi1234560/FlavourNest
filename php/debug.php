3a14a8572a6ddc2298c9a988ccde25cba2a9d89e
odrya-keri-mako <odry.attila@keri.mako.hu>
2026-02-03 12:32:02 +0100

    insert recipe_steps

---
+++ php/debug.php
@@ -0,0 +1,5 @@
+<?php
+
+$_POST['data'] = '{"recipe":{"title":"aaaaaaaaaa","description":"xxxxxxxxxxxxxxxxxxxxxxxxx","servings":4,"prep_time_minutes":6,"author_id":1},"ingredients":[{"ingredient_id":"","quantity":"","unit":""}],"steps":[{"instruction":"aaaaaaaaaaaaaaaaaaa","position":1},{"instruction":"bbbbbbbbbbbbbbbbbbbbbb","position":2},{"instruction":"cccccccccccccccccccccccc","position":3}]}';
+
+require_once('uploadRecipe.php');
