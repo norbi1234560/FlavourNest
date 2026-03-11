80f8648f127ad21649e17e537c36f9bb36a36b60
norbi1234560 <repanorbi.2006@gmail.com>
2026-03-11 12:55:50 +0100

    register validator

--- js/controllers/register.controller.js
+++ js/controllers/register.controller.js
@@ -12,34 +12,44 @@
       
       //register    
       $scope.registerClick=()=>{
-        
-        //current date
-        let date=new Date(),
-        dateYear=date.getFullYear(),
-        dateMonth=date.getMonth(),
-        dateDay=date.getDate(),
-        created_at=`${dateYear}-${(dateMonth+1).toString().padStart(2,"0")}-${dateDay.toString().padStart(2,"0")}`; 
-        
+        $scope.emailBool=false;
+        $scope.nameBool=false;
+        $scope.passwordBool=false;
+        $scope.passwordConfirmBool=false;
 
-        http.request({
-          url:"./php/register.php",
-          data:{  username:$scope.user.username,
-                  email:$scope.user.email,
-                  password:$scope.user.password,
-                  created_at:created_at
-        }})
-        .then(response=>{
-          alert("sikeres regisztráció");
-          $rootScope.user=$scope.user;
-          $rootScope.user.id=response["lastInsertId"];
-          $rootScope.user.created_at=created_at;
-          $rootScope.user.avatar=null;
-          delete $rootScope.user.passwordConfirm;
-          $rootScope.$applyAsync();
-          $state.go("home");
-        })
-        .catch(e=> alert(e))
+        if ($scope.registerForm.$valid &&
+            $scope.user.password===$scope.user.passwordConfirm) {
         
+          //current date
+          let date=new Date(),
+          dateYear=date.getFullYear(),
+          dateMonth=date.getMonth(),
+          dateDay=date.getDate(),
+          created_at=`${dateYear}-${(dateMonth+1).toString().padStart(2,"0")}-${dateDay.toString().padStart(2,"0")}`; 
+          
+
+          http.request({
+            url:"./php/register.php",
+            data:{  username:$scope.user.username,
+                    email:$scope.user.email,
+                    password:$scope.user.password,
+                    created_at:created_at
+          }})
+          .then(response=>{
+            alert("sikeres regisztráció");
+            $rootScope.user=$scope.user;
+            $rootScope.user.id=response["lastInsertId"];
+            $rootScope.user.created_at=created_at;
+            $rootScope.user.avatar=null;
+            delete $rootScope.user.passwordConfirm;
+            $rootScope.$applyAsync();
+            $state.go("home");
+          })
+          .catch(e=> alert(e))
+        }
+        else{
+          alert("helytelen");
+        }
       }
     }
   ]);
